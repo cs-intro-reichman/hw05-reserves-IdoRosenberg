1667c21d7d98af13ed0f71d9a98c56bf2cab967b
Ido Rosenberg <ido.rosenberg@post.runi.ac.il>
2024-02-17 10:31:47 +0200

    Changes for HW5

--- GameOfLife.java
+++ GameOfLife.java
@@ -145,7 +145,9 @@ public class GameOfLife {
     public static void print(int[][] arr) {
 		for(int i=1; i<arr.length-1; i++){
 			for(int j=1; j<arr[i].length-1; j++){
-				System.out.print(arr[i][j] + "  ");
+				System.out.print(arr[i][j]);
+				if(j != arr[i].length-2)
+				System.out.print("  ");
 			}
 			System.out.println();
 			if(i != arr.length-2)
